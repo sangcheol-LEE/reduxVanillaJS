12af068f861b280e4e8eca0d74eeaddde64a94af
이상철 <zlrz6706@gmail.com>
2022-08-09 07:21:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+- 프로젝트 서버 실행하는 법
+
+1. yarn global add parcel-bundler // 안되면 npm install -g parcel-bundler
+2. yarn init -y
+3. parcel index.html
---
+++ main.js
@@ -0,0 +1,74 @@
+import { createStore } from "redux";
+
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// 액션 타입과 액션 생성 함수 정의
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({type: TOGGLE_SWITCH})
+const increase = (difference) => ({type: INCREASE, difference})
+const decrease = () => ({type: DECREASE})
+
+//초기값 설정
+const initialState = {
+  toggle : false,
+  counter : 0
+}
+
+// 리듀서 함수 정의
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_SWITCH :
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE :
+      return {
+        ...state,
+        counter : state.counter + action.difference
+      }
+    case DECREASE :
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default :
+      return state;
+  }
+}
+
+const store = createStore(reducer)
+
+const render = () => {
+  const state = store.getState();
+
+  if(state.toggle) {
+    divToggle.classList.add('active')
+  } else {
+    divToggle.classList.remove("active")
+  }
+
+  counter.innerText = state.counter
+}
+
+render()
+
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch())
+}
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1))
+}
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease())
+}
